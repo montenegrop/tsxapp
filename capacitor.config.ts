19ab6eb878d6c29b396501e8b206d3f14e57626f
montenegrop <pablo.rpm@gmail.com>
2023-01-03 23:23:03 -0300

    codetrix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'someApp',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "someApp",
+  webDir: "build",
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId:
+        "519338495248-726fk8ucg1dpaec3os07p9qjf396d697.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
